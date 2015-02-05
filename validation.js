4fbc288db18bc952d5372f468756f3b54b5c1a29
karenwu1212 <karenwyj2012@gmail.com>
2015-02-05 14:08:29 -0700

    assignment-4

---
+++ validation.js
@@ -0,0 +1,76 @@
+
+		$(document).ready(function(){
+			var btn = document.getElementById('submit');
+			btn.addEventListener('click',function(evt){
+				validation();
+			});
+
+			var phoneInput = document.getElementById('phone');
+			phoneInput.addEventListener('keyup',function(evt){
+				validatePhone1();
+			});
+
+			function validation () {
+				validateName();
+				validateAddress();
+				validatePhone1();
+				validatePhone2();
+				validateEmail();
+			}
+
+			function validateName () {
+				var obj = $('#name');
+				var value = obj.val();
+				if (value.length<=0){
+					obj.css('border-color','red');
+				}
+				else{
+					obj.css('border-color','green');
+				}
+			}
+
+			function validateAddress () {
+				var obj = $('#address');
+				var value = obj.val();
+				if (value.length<=0){
+					obj.css('border-color','red');
+				}
+				else{
+					obj.css('border-color','green');
+				}
+			}
+
+			function validatePhone1() {
+				var obj = $("#phone");
+				var value = obj.val();
+				if(value.length==10){
+					obj.css('border-color','green');
+				}
+				else{
+					obj.css('border-color','red');
+				}
+			}
+
+			function validatePhone2(){
+				var obj = $("#phone");
+				var value = obj.val().toString();
+				if(value.length==10){
+					obj.val("("+value[0]+value[1]+value[2]+")"+value[3]+value[4]+value[5]+"-"+value[6]+value[7]+value[8]+value[9]);
+				}
+			}
+
+			function validateEmail(){
+				var obj = $("#email");
+				var value = obj.val().toString();
+				if(value.indexOf("@")==-1){
+					obj.css('border-color','red');
+				}
+				else{
+					obj.css('border-color','green');
+				}
+			}
+			
+
+		});
+		
+	
